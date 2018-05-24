26f3528d62aab96a89303ee019f44b59470a8554
syntel <chrsitw@g.clemson.edu>
2018-05-24 16:53:43 -0400

    Playing with Google Map using google-map-react

--- src/App.js
+++ src/App.js
@@ -1,46 +1,102 @@
 import React, { Component } from 'react';
-import {Map, GoogleApiWrapper} from 'google-maps-react';
+import {Map, Marker, InfoWindow, GoogleApiWrapper} from 'google-maps-react';
 import logo from './logo.svg';
 import './App.css';
+
+{/* <InfoWindow /> gives the ability to pop up "more info" on the Map */}
 
 class App extends Component {
   constructor(){
     super();
 
+    this.state = {
+      activeMarker: {},
+      selectedPlace: {},
+      showingInfoWindow: false
+    };
   }
 
-  // initMap() {
-  //   // The location of Uluru
-  //   var uluru = {lat: -25.344, lng: 131.036};
-  //   // The map, centered at Uluru
-  //   var map = new google.maps.Map(
-  //       document.getElementById('map'), {zoom: 4, center: uluru});
 
-  //   // The marker, positioned at Uluru
-  //   var marker = new google.maps.Marker({position: uluru, map: map});
-  // }
+  onMouseoverMarker(props, marker, e){
+    console.log("Mousing OVer");
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+  }
+
+  onMarkerClick(props, marker1, e){
+    console.log("showInfo: " + this.state.showingInfoWindow);
+    console.log( props);
+    console.log("marker: " + marker1);
+    
+    
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker1,
+      showingInfoWindow: true
+    });
+  }
   render() {
-    return (
+            {/* initalCenter: Takes an object containing latitude and longitude coordinates. Sets the maps center upon loading. */}
+
+  let showInfo = this.state.showingInfoWindow;
+  if(showInfo){
+    
+  }
+  return (
       <div className="App">
+        <Map className="map"
+          style={{
+            height: "400px",  
+            width: "100%"
+            }}
+            google={this.props.google}
+            initialCenter={{
+              lat: 40.854885,
+              lng: -88.081807 
+            }}
+            zoom={4}
+        >  
 
+        <Marker
+          onClick={(e)=>{this.onMarkerClick(e)}}
+          title={'The marker`s title will appear as a tooltip.'}
+          name={'SOMA'}
+          position={{lat: 37.778519, lng: -122.405640}} 
+        />
+        <Marker
+          onClick={(e)=>{this.onMarkerClick(e)}}
+          onMouseover = {(e)=>{this.onMouseoverMarker(e)}}
+          name={'Dolores park'}
+          position={{lat: 37.759703, lng: -122.428093}} />
+        <Marker />
 
-        <Map 
-        style={{
-            minWwidth: "200px",
-            minHeight: "200px"
+        <Marker
+          onClick={(e)=>{this.onMarkerClick(e)}}
+          onMouseover = {(e)=>{this.onMouseoverMarker(e)}}        
+          title={"This Marker"}
+          position={{lat: 37.762391, lng: -122.439192}}
+          icon={{
+            url: "/img/icon.svg",
+            anchor: this.props.google.maps.Point(32, 32),
+            scaledSize: this.props.google.maps.Size(64, 64)
           }}
-          google={this.props.google}
-          zoom={4}
-      
         />
 
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <InfoWindow 
+          position={{lat: 37.762391, lng: -122.439192}}
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow} 
+        >
+          <div>
+            <h1>{this.state.selectedPlace.name} </h1>
+          </div>
+        </InfoWindow>
+
+
+        </Map>
       </div>
     );
   }
